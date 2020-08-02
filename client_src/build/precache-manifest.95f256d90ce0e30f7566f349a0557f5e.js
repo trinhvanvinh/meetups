1f6ad2b9c94d30c7354e95905599f3e71e7efe8c
trinhvanvinh <trinhvanvinh018@gmail.com>
2020-08-02 17:50:38 +0700

    public prod

---
+++ client_src/build/precache-manifest.95f256d90ce0e30f7566f349a0557f5e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52dc29b18d506e9acd2ae595ac7856ea",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d45698a1c8c2eb1d3ae9",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "33ce8d29167ecabea8ce",
+    "url": "/static/js/2.f08da468.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.f08da468.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d45698a1c8c2eb1d3ae9",
+    "url": "/static/js/main.1e6b758a.chunk.js"
+  },
+  {
+    "revision": "9f2124692b25050f27c2",
+    "url": "/static/js/runtime-main.50b05a76.js"
+  }
+]);
